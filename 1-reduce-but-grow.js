2185c3166dd1edea49c2ed6c94623e6d808d48be
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2024-03-20 12:57:08 +0700

    challenge (1/366)

---
+++ 1-reduce-but-grow.js
@@ -0,0 +1,21 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example: [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+// 1. First Answer
+// function grow(x) {
+//     let result = x[0];
+//     for (let index = 1; index < x.length; index++) {
+//         result = result * x[index];
+//     }
+//     return result;
+// }
+
+// 2. Second Answer
+// function grow(x){
+//     const result = x.reduce((acc, curr) => acc * curr, 1)
+//     return result;
+// }
+
+// 3. Third Answer
+const grow = (x) =>  x.reduce((acc, curr) => acc * curr, 1);
+
+console.log(grow([1,2,3,4]));
